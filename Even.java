3ede03ff6a5f0a0fd9b2c2794da65bf8486cfa80
Pranjali12-wagh <68282209+Pranjali12-wagh@users.noreply.github.com>
2020-07-15 09:07:43 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,11 @@
+public class Even  {
+	public static void main(String[] args) {
+                
+		System.out.println("The Even Numbers are:");
+		for (int i = 1; i <= 10; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i + " ");
+			}
+		}
+	}
+}
---
+++ Loop.java
@@ -0,0 +1,12 @@
+
+ 
+public class Loop
+{
+    public static void main(String[] args)
+    {
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
